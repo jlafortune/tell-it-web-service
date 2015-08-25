7ca94655042fcf870ce8355357f7b91d5f2d0dca
jlafortune <john.m.lafortune@gmail.com>
2015-08-24 20:44:32 -0400

    Changed to Jackson JSON provider; added Maven

--- TellItWebService/src/net/lafortu/tellit/service/ApplicationClass.java
+++ TellItWebService/src/net/lafortu/tellit/service/ApplicationClass.java
@@ -6,6 +6,12 @@ import java.util.Set;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+
 @ApplicationPath("/")
 public class ApplicationClass extends Application {
     @Override
@@ -14,5 +20,22 @@ public class ApplicationClass extends Application {
         // register root resource
         classes.add(TellItService.class);
         return classes;
+    }
+    
+    @Override
+    public Set<Object> getSingletons() {
+      Set<Object> s = new HashSet<Object>();
+      
+      // Register Jackson as JAXB to JSON provider
+      ObjectMapper mapper = new ObjectMapper();
+      AnnotationIntrospector introspector =
+          new JaxbAnnotationIntrospector(mapper.getTypeFactory());   
+      mapper.setAnnotationIntrospector(introspector);
+      
+      JacksonJaxbJsonProvider jaxbProvider = new JacksonJaxbJsonProvider();
+      jaxbProvider.setMapper(mapper);
+      s.add(jaxbProvider);
+      
+      return s;
     }
 }
